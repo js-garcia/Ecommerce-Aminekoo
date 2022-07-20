bf014d1d225df3ce0356c3ea7393ca4c1516ea8b
Soledad Garcia <soledad.j.garcia@gmail.com>
2022-07-19 22:58:09 -0300

    desafio menu ecommerce

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,20 @@
+import './NavBar.scss'
+
+const NavBar = () => {
+    return(
+        <div className='navbar-amineko'>
+            <h1>Logo</h1>
+            <ul>
+                <li><button>Inicio</button></li>
+                <li><button>Productos</button></li>
+                <li><button>Carrito</button></li>
+                <li><button>Contacto</button></li>
+            </ul>
+        </div>
+    )
+
+
+
+}
+
+export default NavBar
